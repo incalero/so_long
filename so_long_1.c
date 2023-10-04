73d1c97a21cd071e3ff5e163b26a69476bb136e0
incalero <130999930+incalero@users.noreply.github.com>
2023-10-04 12:05:55 +0200

    Add files via upload
    ultimo

--- so_long_1.c
+++ so_long_1.c
@@ -6,7 +6,7 @@
 /*   By: incalero <incalero@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 11:57:39 by incalero          #+#    #+#             */
-/*   Updated: 2023/10/03 15:51:13 by incalero         ###   ########.fr       */
+/*   Updated: 2023/10/04 12:05:17 by incalero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,64 +63,69 @@ char **ft_create_array(char *av, int lines)
 		array[i] = s;
 		i++;
     }
-	free(s);
 	array[i] = NULL;
 	close (fd);
 	return (array);
 }
-char **ft_remove_jump(char **array)
+char **ft_remove_jump(int lines, char **array)
 {
-	int i;
-	int j;
-	int	k;
-	char	**clean;
-
-	i = 0;
-	j = 0;
-	k = ft_strlen(array[0]);
-	clean = NULL;
-	while (array[i] != NULL && j < k)
+	int i = 0;
+    char **clean = (char **)malloc(sizeof(char *) * (lines + 1));	
+    if (clean == NULL)
+        return NULL;
+    while (array[i] != NULL) 
 	{
-		clean[i][j] = array[i][j];
-		j++;
+        int len = strlen(array[i]);
+        int j = 0;
+        int k = 0;
+        clean[i] = (char *)malloc(len + 1);
+		if (clean[i] == NULL) 
+            return NULL;
+        while (j < len) 
+		{
+            if (array[i][j] != '\n') 
+				clean[i][k++] = array[i][j];
+            j++;
+        }
+		clean[i][k] = '\0'; 
 		i++;
 	}
-	printf("el array 1 es = %s\n", array[0]);
-	printf("el array 2 es = %s\n", array[1]);
-	printf("el array 3 es = %s\n", array[2]);
-	printf("el array 4 es = %s\n", array[3]);
-	printf("el array 5 es = %s\n", array[4]);
-	printf("el array 6 es = %s\n", array[5]);
-	printf("el array 7 es = %s\n", array[6]);
-	printf("el array 8 es = %s\n", array[7]);
-	printf("el array 9 es = %s\n", array[8]);
-	printf("el array 10 es = %s\n", array[9]);
-	printf("el array 11 es = %s\n", array[10]);
-	printf("el array 12 es = %s\n", array[11]);
-	printf("el array 13 es = %s\n", array[12]);
-	printf("el array 14 es = %s\n", array[13]);
-		
-	printf("k = %d\n", k);
+	clean[i] = NULL;
 	return (clean);
 }
+
 int ft_correct_map(int lines, char **array)
 {
 	int	i;
 	int j;
 	
 	i = 0;
-	j = 1;
+	j = i + 1;
 	while (array[i] != NULL)
 	{
-		if (lines > 4)
+		if (lines < 4)
 			return (0);
-		while (ft_strlen(array[i++]) != ft_strlen(array[j++]))
-			return (0);
+		/* if ((i < lines) && (ft_strlen(array[i]) != ft_strlen(array[j])))
+			return (0); 
+		i++;*/
+		printf("lines = %d\n", lines);
+		printf("el clean 1 es = %s\n", array[0]);
+		printf("el clean 1 es = %s\n", array[lines -1]);
+		while (array[i][i] != '\0')
+		{
+			if ((array[0][i] != 1 && array[lines -1][i] != 1))
+				return (0); 
+		}
+		i++;
+		
 	}
-	return(0);
+	return(1);
 }
+
+
 int	main(int ac, char **av)
 {
+	//int 	i;
 	int		lines;
 	char	**array;
 	char	**clean;
@@ -137,26 +142,33 @@ int	main(int ac, char **av)
 	lines = ft_lines_count(fd);
 	close(fd);
 	array = ft_create_array(av[1], lines);
-	if (array == NULL) 
-		return (write (1, "Error creating the array\n", 25), -1);
-	clean = ft_remove_jump(array);
+	clean = ft_remove_jump(lines, array);
+	printf ("correct map = %d\n", ft_correct_map(lines, clean));
+	if (!ft_correct_map(lines, clean))
+		return(write (1, "incorrect map\n", 60), -1);
 	
-	printf("el len del array 1 es %d\n", ft_strlen(array[0]));
-	printf("el len del array 14 es %d\n", ft_strlen(array[13]));
+	/* i = 0;
+	while (array[i] != NULL)
+		free(array[i++]);
+	free(array);  */
 	
-	/* printf("el array 1 es = %s\n", array[0]);
-	printf("el array 2 es = %s\n", array[1]);
-	printf("el array 3 es = %s\n", array[2]);
-	printf("el array 4 es = %s\n", array[3]);
-	printf("el array 5 es = %s\n", array[4]);
-	printf("el array 6 es = %s\n", array[5]);
-	printf("el array 7 es = %s\n", array[6]);
-	printf("el array 8 es = %s\n", array[7]);
-	printf("el array 9 es = %s\n", array[8]);
-	printf("el array 10 es = %s\n", array[9]);
-	printf("el array 11 es = %s\n", array[10]);
-	printf("el array 12 es = %s\n", array[11]);
-	printf("el array 13 es = %s\n", array[12]);
-	printf("el array 14 es = %s\n", array[13]); */
+	
+	/* printf("el clean 1 es = %s\n", clean[0]);
+	printf("el clean 2 es = %s\n", clean[1]);
+	printf("el clean 3 es = %s\n", clean[2]);
+	printf("el clean 4 es = %s\n", clean[3]);
+	printf("el clean 5 es = %s\n", clean[4]);
+	printf("el clean 6 es = %s\n", clean[5]);
+	printf("el clean 7 es = %s\n", clean[6]);
+	printf("el clean 8 es = %s\n", clean[7]);
+	printf("el clean 9 es = %s\n", clean[8]);
+	printf("el clean 10 es = %s\n", clean[9]);
+	printf("el clean 11 es = %s\n", clean[10]);
+	printf("el clean 12 es = %s\n", clean[11]);
+	printf("el clean 13 es = %s\n", clean[12]);
+	printf("el clean 14 es = %s\n", clean[13]); */
+	
+
+
 	return (0);
 }
