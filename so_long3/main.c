07f3a0bcc75a13d8c54efdfc7099ade02340fc23
incalero <130999930+incalero@users.noreply.github.com>
2023-10-23 19:33:43 +0200

    Add files via upload
    gffgfgfg

---
+++ so_long3/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 11:23:03 by incalero          #+#    #+#             */
+/*   Updated: 2023/10/23 19:21:53 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (write(1, "Argument Error\n", 15), -1); 
+	if (!ft_file_type(av[1]))
+		return (write(1, "incorrect file type\n", 20), -1);
+	if(!ft_good_map(av))
+		return (write(1, "imposible to play\n", 18), -1);
+	//ft_play(ft_good_map(av));
+	return (0);
+	
+}
